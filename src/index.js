a32408de8acc16d473b97612479180bebf50e84b
Moises Munoz <moises_a_munoz@yahoo.com>
2018-05-02 19:26:29 -0700

    Changed search_bar to be a smart component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import KEY from './api_key';
+
+// import KEY from './api_key';
+import SearchBar from './components/search_bar';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
